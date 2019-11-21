bf32c0844a133fb0958cfbc19a3ee6b377f5ff37
yishibakaien <646932161@qq.com>
2019-11-21 08:11:57 +0000

    Deploying to gh-pages from master cb467730897fd671fd68ad19520fb255b05c37ed

---
+++ precache-manifest.67759f683c4ed8b24ae4f079da911b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27131999114a2eb2020702cfe39e1b40",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "5f0e962971242cb178de",
+    "url": "/github-actions-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "555149349d442c73b1c4",
+    "url": "/github-actions-demo/static/js/2.dbe21ac2.chunk.js"
+  },
+  {
+    "revision": "5f0e962971242cb178de",
+    "url": "/github-actions-demo/static/js/main.38186d6f.chunk.js"
+  },
+  {
+    "revision": "c448e9729eca4ab15cc9",
+    "url": "/github-actions-demo/static/js/runtime-main.bab7c5fc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-actions-demo/static/media/logo.25bf045c.svg"
+  }
+]);
